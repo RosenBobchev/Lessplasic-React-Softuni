655eeac3dcce2f42bbf138bcfd62fab96e69484a
Rosen <rosenbobchev@abv.bg>
2019-12-05 16:11:08 +0200

    Listing all articles in database

--- lessplastic/src/components/Article/AllArticles.js
+++ lessplastic/src/components/Article/AllArticles.js
@@ -1,25 +1,33 @@
 import React, { Component } from 'react';
 import {Container} from "react-bootstrap";
 import Article from "./Article";
+import articleService from "../../services/articleService";
+import Col from "react-bootstrap/Col";
 
 export default class AllArticles extends Component {
 
+    constructor(props){
+        super(props);
+
+        this.state = {
+            articlesFromDatabase: []
+        }
+    }
+    componentDidMount() {
+        articleService.getAllArticles().then((data) => this.setState({articlesFromDatabase: data.data}));
+    }
+
     render() {
+        const {articlesFromDatabase} = this.state;
+        const articles = articlesFromDatabase.map(a => (
+            <div key={a._id} style={{marginTop: '10px', marginBottom: '10px', width: '80%', display: 'inline-block'}}>
+                <Article articleProps={a}/>
+            </div>));
+
         return (
             <Container>
                 <div style={{textAlign: 'center'}}>
-                    <div style={{marginTop: '10px', marginBottom: '10px', width: '80%', display: 'inline-block'}}>
-                    <Article />
-                    </div>
-                    <div style={{marginTop: '10px', marginBottom: '10px', width: '80%', display: 'inline-block'}}>
-                        <Article />
-                    </div>
-                    <div style={{marginTop: '10px', marginBottom: '10px', width: '80%', display: 'inline-block'}}>
-                        <Article />
-                    </div>
-                    <div style={{marginTop: '10px', marginBottom: '10px', width: '80%', display: 'inline-block'}}>
-                        <Article />
-                    </div>
+                    {articles}
                 </div>
             </Container>
         )
